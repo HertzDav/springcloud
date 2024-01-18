677469943e5afbf7bfc5606eb44b3b79a8bff554
Hertz <1051838030@qq.com>
2024-01-18 23:10:00 +0800

    新建项目

---
+++ src/main/java/hertzdav/springcloud/springcloudalibaba/SpringCloudAliBabaApplication.java
@@ -0,0 +1,13 @@
+package hertzdav.springcloud.springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAliBabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAliBabaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hertzdav/springcloud/springcloudalibaba/SpringCloudAliBabaApplicationTests.java
@@ -0,0 +1,13 @@
+package hertzdav.springcloud.springcloudalibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAliBabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
